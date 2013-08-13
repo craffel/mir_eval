eedd645705144009e1ffdd330f465e12f85e777d
Dawen Liang <dl2771@columbia.edu>
2013-08-13 12:35:05 -0700

    place holder for separation evalution

---
+++ separation.py
@@ -0,0 +1,8 @@
+'''
+CREATED: 2013-08-13 12:31:25 by Dawen Liang <daliang@adobe.com>
+
+Source separation evaluation:
+    BSS-EVAL -- SDR (Source-to-Distortion Ratio), SIR (Source-to-Interferences
+                Ratio), and SAR (Source-to-Artifacts Ratio)
+
+'''
