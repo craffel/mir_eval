f69c769a38002a709da4c395e0dd4c6251d582c7
Brian McFee <brm2132@columbia.edu>
2013-10-21 12:30:23 -0400

    added a segment annotation importer

--- mir_eval/util.py
+++ mir_eval/util.py
@@ -33,3 +33,55 @@ def multiline(filename, sep='\t'):
         for line in f:
             yield np.fromstring(line, sep=sep)
     pass
+
+def import_segment_boundaries(filename, cols=[0,1], sep=None, t_max=None):
+    '''Import segment boundaries from an annotation file.  
+    In typical MIREX fashion, annotations are formatted as:
+
+    START_1     END_1   LABEL_1
+    START_2     END_2   LABEL_2
+    ...
+
+    where START_* and END_* are given in seconds.
+    
+    We assume that END_i = START_i+1.
+
+    :parameters:
+        - filename : string
+            Path to the annotation file
+
+        - cols : list of ints
+            Which columns of the file specify the boundaries
+
+        - sep : string or None
+            Field delimiter. See `numpy.loadtxt()` for details.
+
+        - t_max : float or None
+            If provided, the boundaries will be truncated or expanded 
+            to the given time. This is useful for correcting incomplete
+            annotations against a ground-truth reference.
+
+    :returns:
+        - boundaries : np.array
+            List of all segment boundaries
+    '''
+
+    # Load the input
+    values = np.loadtxt(filename, usecols=cols, delimiter=sep)
+
+    # Flatten out the boundaries, remove dupes
+    values = np.unique(values.flatten())
+
+    # Does it start at time 0?
+    if values[0] > 0:
+        values = np.concatenate( ([0.0], values))
+
+    if t_max is not None:
+        # Make sure we're not past the end of the track
+        values = values[values <= t_max]
+
+        # Pad out with a silence segment if undercomplete
+        if values[-1] < t_max:
+            values = np.concatenate((values, [t_max]))
+
+    return values
