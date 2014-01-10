8ad4d572fc56872ff7b743749809d2b51d8fbc88
Dawen Liang <dl2771@columbia.edu>
2014-01-10 10:34:46 +0800

    Use numpy.linalg.solve instead of numpy.linalg.lstsq to solve square matrix linear system

--- mir_eval/separation.py
+++ mir_eval/separation.py
@@ -141,7 +141,7 @@ def _project(estimated_source, sources, flen):
 
     ## Computing projection ##
     # Distortion filters
-    C = np.linalg.lstsq(G, D)[0].reshape(flen, nsrc, order='F')
+    C = np.linalg.solve(G, D).reshape(flen, nsrc, order='F')
     # Filtering
     sproj = np.zeros(nsampl + flen - 1)
     for i in xrange(nsrc):
