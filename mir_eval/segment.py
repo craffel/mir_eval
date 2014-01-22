872dcc72e7dc6f3182d4fee3197f2fe1e6a0cfaf
Brian McFee <brm2132@columbia.edu>
2014-01-22 12:30:42 -0500

    simplified nce code

--- mir_eval/segment.py
+++ mir_eval/segment.py
@@ -324,26 +324,23 @@ def frame_clustering_nce(annotated_boundaries, predicted_boundaries, frame_size=
     if len(y_true) != len(y_pred):
         raise ValueError('Timing mismatch: %.3f vs %.3f' % (annotated_boundaries[-1], predicted_boundaries[-1]))
 
-    # Make the contingency table
+    # Make the contingency table: shape = (n_true, n_pred)
     contingency = metrics.contingency_matrix(y_true, y_pred).astype(float)
 
-    n_frames = len(y_true)
-    n_true, n_pred = contingency.shape
-
     # Compute the marginals
-    p_pred = contingency.sum(axis=0) / n_frames
-    p_true = contingency.sum(axis=1) / n_frames
+    p_pred = contingency.sum(axis=0) / len(y_true) 
+    p_true = contingency.sum(axis=1) / len(y_true)
 
     true_given_pred = p_pred.dot(scipy.stats.entropy(contingency,   base=2))
     pred_given_true = p_true.dot(scipy.stats.entropy(contingency.T, base=2))
 
     score_over = 0.0
-    if n_pred > 1:
-        score_over  = 1. - pred_given_true / np.log2(n_pred)
+    if contingency.shape[1] > 1:
+        score_over  = 1. - pred_given_true / np.log2(contingency.shape[1])
 
     score_under = 0.0
-    if n_true > 1:
-        score_under = 1. - true_given_pred / np.log2(n_true)
+    if contingency.shape[0] > 1:
+        score_under = 1. - true_given_pred / np.log2(contingency.shape[0])
 
     f_measure = util.f_measure(score_over, score_under, beta=beta)
 
