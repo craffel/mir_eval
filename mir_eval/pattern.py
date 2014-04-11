f9546e3500f447f417c7b72544f0dc8f3e4eaf37
Uri Nieto <oriol.nieto@gmail.com>
2014-04-10 21:07:42 -0400

    Started working on pattern discovery evaluation

---
+++ mir_eval/pattern.py
@@ -0,0 +1,39 @@
+'''Functions for evaluating the task of pattern discovery.
+
+Input Format
+============
+
+Set of patterns including all their occurrences.
+
+Metrics implemented
+===================
+
+Standard Precision, Recall and F1 Score
+---------------------------------------
+
+Strict metric in order to find the exact patterns.
+
+Establishment Precision, Recall and F1 Score
+--------------------------------------------
+
+
+
+
+
+
+Metrics developed by Tom Colins and David Meredith.
+
+Written by Oriol Nieto (oriol@nyu.edu), 2014
+'''
+
+
+import numpy as np
+
+
+def standard_metrics(reference_patterns, estimated_patterns):
+    '''Standard Precision, Recall and F1 Score
+    '''
+
+def load_patternns(filename):
+    '''TODO'''
+    pass
