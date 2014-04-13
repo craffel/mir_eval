d914c255d06f99e40b2908edc9f7b04a82e771d6
Uri Nieto <oriol.nieto@gmail.com>
2014-04-13 15:14:57 -0400

    Added validation function for patterns

--- tests/test_pattern.py
+++ tests/test_pattern.py
@@ -1,15 +1,20 @@
 """
+Some unit tests for the pattern discovery task.
+
+Written by Oriol Nieto (oriol@nyu.edu), 2014
 """
 
 import unittest
-import numpy as np
 from mir_eval import pattern, input_output
 
 
 class PatternTests(unittest.TestCase):
 
     def setUp(self):
-        pass
+        self.ref_P = input_output.load_patterns(
+            "data/pattern/reference-poly.txt")
+        self.est_P = input_output.load_patterns(
+            "data/pattern/estimate-poly.txt")
 
     def tearDown(self):
         pass
@@ -26,18 +31,21 @@ class PatternTests(unittest.TestCase):
 
     def test_standard_FPR(self):
         delta = 1e-3
-        ref_P = input_output.load_patterns("data/pattern/reference-poly.txt")
-        est_P = input_output.load_patterns("data/pattern/estimate-poly.txt")
-        F, P, R = pattern.standard_FPR(ref_P, est_P)
+        F, P, R = pattern.standard_FPR(self.ref_P, self.est_P)
         self.assertAlmostEqual(F, 0.28571, delta=delta)
         self.assertAlmostEqual(P, 0.25, delta=delta)
         self.assertAlmostEqual(R, 0.33333, delta=delta)
 
+    def test_establishment_FPR(self):
+        delta = 1e-3
+        #F, P, R = pattern.establishment_FPR(self.ref_P, self.est_P)
+        #self.assertAlmostEqual(F, 0.25249, delta=delta)
+        #self.assertAlmostEqual(P, 0.24606, delta=delta)
+        #self.assertAlmostEqual(R, 0.25927, delta=delta)
+
     def test_three_layer_FPR(self):
         delta = 1e-3
-        ref_P = input_output.load_patterns("data/pattern/reference-poly.txt")
-        est_P = input_output.load_patterns("data/pattern/estimate-poly.txt")
-        F, P, R = pattern.three_layer_FPR(ref_P, est_P)
+        F, P, R = pattern.three_layer_FPR(self.ref_P, self.est_P)
         self.assertAlmostEqual(F, 0.10211, delta=delta)
         self.assertAlmostEqual(P, 0.09465, delta=delta)
         self.assertAlmostEqual(R, 0.11085, delta=delta)
