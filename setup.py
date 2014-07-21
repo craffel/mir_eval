922f153a9ab1b93ed9b088e559fabe8c8d1143ec
Colin Raffel <craffel@gmail.com>
2014-07-21 19:22:58 -0400

    PEP8 cleanup

--- setup.py
+++ setup.py
@@ -3,20 +3,21 @@ from setuptools import setup
 setup(
     name='mir_eval',
     version='0.0.0',
-    description='Common accuracy scores for common audio/music processing tasks.',
+    description='Common metrics for common audio/music processing tasks.',
     author='Colin Raffel',
     author_email='craffel@gmail.com',
     url='https://github.com/craffel/mir_eval',
     packages=['mir_eval'],
     long_description="""\
-    Collection of Python scripts to compute common heuristic accuracy scores for various music/audio information retrieval/signal processing tasks.
+    Collection of Python scripts to compute common heuristic accuracy scores
+    for various music/audio information retrieval/signal processing tasks.
     """,
     classifiers=[
-          "License :: OSI Approved :: GNU General Public License (GPL)",
-          "Programming Language :: Python",
-          "Development Status :: 3 - Alpha",
-          "Intended Audience :: Developers",
-          "Topic :: Multimedia :: Sound/Audio :: Analysis",
+        "License :: OSI Approved :: GNU General Public License (GPL)",
+        "Programming Language :: Python",
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "Topic :: Multimedia :: Sound/Audio :: Analysis",
     ],
     keywords='audio music mir dsp',
     license='GPL',
