e43dc3f9999405f355f18c80b2bb7135d37f9133
Colin Raffel <craffel@gmail.com>
2014-07-22 15:53:45 -0400

    PEP8/Pylint/Pyflakes cleanup, and removing use of METRICS dictionary

--- evaluators/melody_eval.py
+++ evaluators/melody_eval.py
@@ -16,12 +16,12 @@ from Polyphonic Music Signals: Approaches, Applications and Challenges",
 IEEE Signal Processing Magazine, 31(2):118-134, Mar. 2014.
 '''
 
-import numpy as np
 import argparse
 import sys
 import os
 from collections import OrderedDict
 import mir_eval
+
 
 def evaluate(reference_file, estimated_file, hop=None):
     '''
@@ -29,26 +29,31 @@ def evaluate(reference_file, estimated_file, hop=None):
     treated as the reference (ground truth) and the second as the estimate to
     be evaluated (prediction).
 
-    Input:
-        truth_file - path to reference file. File should contain 2 columns:
-              column 1 with timestamps and column 2 the corresponding reference
-              frequency values in Hz (see *).
-        prediction_file - path to estimate file. File should contain with 2 columns:
-              column 1 with timestamps and column 2 the corresponding estimate
-              frequency values in Hz (see **).
-        hop - the desired hop size (in seconds) to compare the reference and
-              estimate sequences (see ***)
+    :parameters:
+        - truth_file : str
+            path to reference file. File should contain 2 columns:
+            column 1 with timestamps and column 2 the corresponding reference
+            frequency values in Hz (see *).
+        - prediction_file : str
+            path to estimate file. File should contain with 2
+            columns: column 1 with timestamps and column 2 the corresponding
+            estimate frequency values in Hz (see **).
+        - hop : float
+            the desired hop size (in seconds) to compare the reference and
+            estimate sequences (see ***)
 
-    Output:
-        M - ordered dictionary containing 5 evaluation measures:
-        voicing recall rate -       Fraction of voiced frames in ref estimated as voiced in est
-        voicing false alarm rate -  Fraction of unvoiced frames in ref estimated as voiced in est
-        raw pitch -                 Fraction of voiced frames in ref for which
-                                    est gives a correct pitch estimate (within 50 cents)
-        raw chroma -                Same as raw pitch, but ignores octave errors
-        overall accuracy -          Overall performance measure combining pitch and voicing
-
-
+    :returns:
+        - M : collections.OrderedDict
+            ordered dictionary containing 5 evaluation measures:
+            voicing recall rate - Fraction of voiced frames in ref estimated as
+                                  voiced in est
+            voicing false alarm rate - Fraction of unvoiced frames in ref
+                                       estimated as voiced in est
+            raw pitch - Fraction of voiced frames in ref for which
+                        est gives a correct pitch estimate (within 50 cents)
+            raw chroma - Same as raw pitch, but ignores octave errors
+            overall accuracy - Overall performance measure combining pitch and
+                               voicing
 
     *    Unvoiced frames should be indicated by 0 Hz.
     **   Unvoiced frames can be indicated either by 0 Hz or by a negative Hz
@@ -64,22 +69,29 @@ def evaluate(reference_file, estimated_file, hop=None):
     ref_time, ref_freq = mir_eval.io.load_time_series(reference_file)
     est_time, est_freq = mir_eval.io.load_time_series(estimated_file)
     # Convert to reference/estimated voicing/frequency (cent) arrays
-    ref_voicing, est_voicing, ref_cent, est_cent = mir_eval.melody.to_cent_voicing(ref_time,
-                                                                                ref_freq,
-                                                                                est_time,
-                                                                                est_freq,
-                                                                                hop=hop)
+    ref_voicing, est_voicing, ref_cent, est_cent = \
+        mir_eval.melody.to_cent_voicing(ref_time, ref_freq, est_time, est_freq,
+                                        hop=hop)
 
     # Compute metrics
     M = OrderedDict()
 
-    for name, metric in mir_eval.melody.METRICS.items():
-        if metric == mir_eval.melody.voicing_measures:
-            M[name] = metric(ref_voicing, est_voicing)
-        else:
-            M[name] = metric(ref_voicing, est_voicing, ref_cent, est_cent)
-
+    M['Voicing Measures'] = mir_eval.melody.voicing_measures(ref_voicing,
+                                                             est_voicing)
+    M['Raw Pitch Accuracy'] = mir_eval.melody.raw_pitch_accuracy(ref_voicing,
+                                                                 est_voicing,
+                                                                 ref_cent,
+                                                                 est_cent)
+    M['Raw Chroma Accuracy'] = mir_eval.melody.raw_chroma_accuracy(ref_voicing,
+                                                                   est_voicing,
+                                                                   ref_cent,
+                                                                   est_cent)
+    M['Overall Accuracy'] = mir_eval.melody.raw_pitch_accuracy(ref_voicing,
+                                                               est_voicing,
+                                                               ref_cent,
+                                                               est_cent)
     return M
+
 
 def print_evaluation(prediction_file, M):
     '''
@@ -94,15 +106,16 @@ def print_evaluation(prediction_file, M):
 def process_arguments():
     '''Argparse function to get the program parameters'''
 
-    parser = argparse.ArgumentParser(description='mir_eval melody extraction evaluation')
+    parser = argparse.ArgumentParser(description='mir_eval melody extraction '
+                                                 'evaluation')
 
     parser.add_argument('reference_file',
-                        action = 'store',
-                        help = 'path to the ground truth annotation')
+                        action='store',
+                        help='path to the ground truth annotation')
 
     parser.add_argument('estimated_file',
-                        action = 'store',
-                        help = 'path to the estimation file')
+                        action='store',
+                        help='path to the estimation file')
 
     parser.add_argument("--hop", type=float,
                     help="hop size (in seconds) to use for the evaluation")
