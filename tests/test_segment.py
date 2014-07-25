d41316536de3a3d8001af2bd57178b1c07a3c973
Colin Raffel <craffel@gmail.com>
2014-07-25 12:43:09 -0400

    Updating load_intervals to load_labeled_intervals

--- tests/test_segment.py
+++ tests/test_segment.py
@@ -23,8 +23,8 @@ def generate_data():
     sco_files = sorted(glob.glob(SCORES_GLOB))
 
     for ref_f, est_f, sco_f in zip(ref_files, est_files, sco_files):
-        ref_t, ref_l = mir_eval.io.load_intervals(ref_f)
-        est_t, est_l = mir_eval.io.load_intervals(est_f)
+        ref_t, ref_l = mir_eval.io.load_labeled_intervals(ref_f)
+        est_t, est_l = mir_eval.io.load_labeled_intervals(est_f)
 
         with open(sco_f, 'r') as f:
             scores = json.load(f)
