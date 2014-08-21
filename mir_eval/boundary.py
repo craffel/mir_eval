54258b3ce5f2a9f8f186c6577f8826648f6cbed3
Colin Raffel <craffel@gmail.com>
2014-08-21 14:46:53 -0400

    Can use either io function

--- mir_eval/boundary.py
+++ mir_eval/boundary.py
@@ -19,12 +19,14 @@ def validate(reference_intervals, estimated_intervals, trim):
 
     :parameters:
         - reference_intervals : np.ndarray, shape=(n, 2)
-            reference segment intervals,
-            as returned by `mir_eval.io.load_intervals`
+            reference segment intervals, as returned by
+            `mir_eval.io.load_intervals` or
+            `mir_eval.io.load_labeled_intervals`.
 
         - estimated_intervals : np.ndarray, shape=(m, 2)
-            estimated segment intervals,
-            as returned by `mir_eval.io.load_intervals`
+            estimated segment intervals, as returned by
+            `mir_eval.io.load_intervals` or
+            `mir_eval.io.load_labeled_intervals`.
 
         - trim : bool
             will the start and end events be trimmed?
@@ -76,12 +78,14 @@ def detection(reference_intervals, estimated_intervals,
 
     :parameters:
         - reference_intervals : np.ndarray, shape=(n, 2)
-            reference segment intervals,
-            as returned by `mir_eval.io.load_intervals`
+            reference segment intervals, as returned by
+            `mir_eval.io.load_intervals` or
+            `mir_eval.io.load_labeled_intervals`.
 
         - estimated_intervals : np.ndarray, shape=(m, 2)
-            estimated segment intervals,
-            as returned by `mir_eval.io.load_intervals`
+            estimated segment intervals, as returned by
+            `mir_eval.io.load_intervals` or
+            `mir_eval.io.load_labeled_intervals`.
 
         - window : float > 0
             size of the window of 'correctness' around ground-truth beats
@@ -144,12 +148,14 @@ def deviation(reference_intervals, estimated_intervals, trim=False):
 
     :parameters:
         - reference_intervals : np.ndarray, shape=(n, 2)
-            reference segment intervals,
-            as returned by `mir_eval.io.load_intervals`
+            reference segment intervals, as returned by
+            `mir_eval.io.load_intervals` or
+            `mir_eval.io.load_labeled_intervals`.
 
         - estimated_intervals : np.ndarray, shape=(m, 2)
-            estimated segment intervals,
-            as returned by `mir_eval.io.load_intervals`
+            estimated segment intervals, as returned by
+            `mir_eval.io.load_intervals` or
+            `mir_eval.io.load_labeled_intervals`.
 
         - trim : boolean
             if ``True``, the first and last intervals are ignored.
