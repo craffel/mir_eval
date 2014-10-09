e22a6ad01632a1932f222ec8d8f9062b6fa960cc
Dan Ellis <dpwe@ee.columbia.edu>
2014-10-08 21:21:31 -0400

    fix to sphinx: conf.py - include .. in sys.path

--- docs/conf.py
+++ docs/conf.py
@@ -18,7 +18,7 @@ import os
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
-#sys.path.insert(0, os.path.abspath('.'))
+sys.path.insert(0, os.path.abspath('..'))
 
 # -- General configuration ------------------------------------------------
 
@@ -259,3 +259,5 @@ texinfo_documents = [
 
 # If true, do not generate a @detailmenu in the "Top" node's menu.
 #texinfo_no_detailmenu = False
+
+autodoc_member_order = 'bysource'
