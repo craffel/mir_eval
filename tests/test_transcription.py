6016e6f1392cc45f1807394ea42ee3c296dc8656
Justin Salamon <justin.salamon@gmail.com>
2016-02-16 15:23:38 -0500

    replace load funcs for unit test data with file-level contants

--- tests/test_transcription.py
+++ tests/test_transcription.py
@@ -5,48 +5,33 @@ import numpy as np
 
 A_TOL = 1e-12
 
+REF = np.array([
+    [0.100, 0.300, 220.000],
+    [0.300, 0.400, 246.942],
+    [0.500, 0.600, 277.183],
+    [0.550, 0.650, 293.665]])
 
-def _load_unit_test_reference():
+EST = np.array([
+    [0.120,   0.290,   225.000],
+    [0.300,   0.350,   246.942],
+    [0.500,   0.600,   500.000],
+    [0.550,   0.600,   293.665],
+    [0.560,   0.650,   293.665]])
 
-    ref = np.array([
-        [0.100, 0.300, 220.000],
-        [0.300, 0.400, 246.942],
-        [0.500, 0.600, 277.183],
-        [0.550, 0.650, 293.665]])
-
-    return ref[:, :2], ref[:, 2]
-
-
-def _load_unit_test_estimate():
-
-    est = np.array([
-            [0.120,   0.290,   225.000],
-            [0.300,   0.350,   246.942],
-            [0.500,   0.600,   500.000],
-            [0.550,   0.600,   293.665],
-            [0.560,   0.650,   293.665]])
-
-    return est[:, :2], est[:, 2]
-
-
-def _load_unit_test_scores():
-
-    scores = {
-        "Precision": 0.4,
-        "Recall": 0.5,
-        "F-measure": 0.4444444444444445,
-        "Precision_no_offset": 0.6,
-        "Recall_no_offset": 0.75,
-        "F-measure_no_offset": 0.6666666666666665
-    }
-
-    return scores
+SCORES = {
+    "Precision": 0.4,
+    "Recall": 0.5,
+    "F-measure": 0.4444444444444445,
+    "Precision_no_offset": 0.6,
+    "Recall_no_offset": 0.75,
+    "F-measure_no_offset": 0.6666666666666665
+}
 
 
 def test_match_notes():
 
-    ref_int, ref_pitch = _load_unit_test_reference()
-    est_int, est_pitch = _load_unit_test_estimate()
+    ref_int, ref_pitch = REF[:,:2], REF[:,2]
+    est_int, est_pitch = EST[:,:2], EST[:,2]
 
     matching = \
         mir_eval.transcription.match_notes(ref_int, ref_pitch, est_int,
@@ -64,18 +49,16 @@ def test_match_notes():
 def test_precision_recall_f1():
 
     # load test data
-    ref_int, ref_pitch = _load_unit_test_reference()
-    est_int, est_pitch = _load_unit_test_estimate()
-
-    scores = _load_unit_test_scores()
+    ref_int, ref_pitch = REF[:,:2], REF[:,2]
+    est_int, est_pitch = EST[:,:2], EST[:,2]
 
     precision, recall, f_measure = \
         mir_eval.transcription.precision_recall_f1(ref_int, ref_pitch, est_int,
                                                    est_pitch)
 
     scores_gen = np.array([precision, recall, f_measure])
-    scores_exp = np.array([scores['Precision'], scores['Recall'],
-                           scores['F-measure']])
+    scores_exp = np.array([SCORES['Precision'], SCORES['Recall'],
+                           SCORES['F-measure']])
     assert np.allclose(scores_exp, scores_gen, atol=A_TOL)
 
     precision, recall, f_measure = \
@@ -84,7 +67,7 @@ def test_precision_recall_f1():
                                                    offset_ratio=None)
 
     scores_gen = np.array([precision, recall, f_measure])
-    scores_exp = np.array([scores['Precision_no_offset'],
-                           scores['Recall_no_offset'],
-                           scores['F-measure_no_offset']])
+    scores_exp = np.array([SCORES['Precision_no_offset'],
+                           SCORES['Recall_no_offset'],
+                           SCORES['F-measure_no_offset']])
     assert np.allclose(scores_exp, scores_gen, atol=A_TOL)
