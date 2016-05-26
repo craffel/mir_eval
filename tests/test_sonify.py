2667602f0b6c531be5c20db118b154006edf7e3d
Brian McFee <brian.mcfee@nyu.edu>
2016-05-26 13:28:51 -0400

    implemented pitch contour sonification.
    stupid pep8
    
    minor efficiency improvements in pitch_contour sonification
    
    fixed the silly test case in pitch_contour sonify
    
    updated test comments in pitch_contour sonification
    
    pep8 is stupid

--- tests/test_sonify.py
+++ tests/test_sonify.py
@@ -2,6 +2,7 @@
 
 import mir_eval
 import numpy as np
+import scipy
 
 
 def test_clicks():
@@ -53,3 +54,37 @@ def test_chords():
             ['C', 'C:maj', 'D:min7', 'E:min', 'C#', 'C', 'C', 'C', 'C', 'C'],
             intervals, fs, length=fs*11)
         assert len(signal) == 11*fs
+
+
+def test_pitch_contour():
+
+    # Generate some random pitch
+    fs = 8000
+    times = np.linspace(0, 5, num=5 * fs, endpoint=True)
+
+    noise = scipy.ndimage.gaussian_filter1d(np.random.randn(len(times)),
+                                            sigma=256)
+    freqs = 440.0 * 2.0**(16 * noise)
+
+    # negate a bunch of sequences
+    idx = np.unique(np.random.randint(0, high=len(times), size=32))
+    for start, end in zip(idx[::2], idx[1::2]):
+        freqs[start:end] *= -1
+
+    # Test with inferring duration
+    x = mir_eval.sonify.pitch_contour(times, freqs, fs)
+    assert len(x) == fs * 5
+
+    # Test with an explicit duration
+    # This forces the interpolator to go off the end of the sampling grid,
+    # which should result in a constant sequence in the output
+    x = mir_eval.sonify.pitch_contour(times, freqs, fs, length=fs * 7)
+    assert len(x) == fs * 7
+    assert np.allclose(x[-fs * 2:], x[-fs * 2])
+
+    # Test with an explicit duration and a fixed offset
+    # This forces the interpolator to go off the beginning of
+    # the sampling grid, which should result in a constant output
+    x = mir_eval.sonify.pitch_contour(times + 5.0, freqs, fs, length=fs * 7)
+    assert len(x) == fs * 7
+    assert np.allclose(x[:fs * 5], x[0])
