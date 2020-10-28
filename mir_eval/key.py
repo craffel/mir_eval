ea8585c32eef5351d3d82e0cdde97db4b7638413
Pedro <pppramonedafranco@hotmail.com>
2020-10-28 20:52:16 +0100

    Update lower() proposal

--- mir_eval/key.py
+++ mir_eval/key.py
@@ -140,8 +140,7 @@ def weighted_score(reference_key, estimated_key):
         return 1.
     # If reference or estimated key are x and they are not the same key
     # then the result is 'Other'.
-    if reference_key == 'X' or estimated_key == 'X' \
-            or reference_key == 'x' or estimated_key == 'x':
+    if reference_key.lower() == 'x' or estimated_key.lower() == 'x':
         return 0.
     # If keys are the same mode and a perfect fifth (differ by 7 semitones)
     if (estimated_mode == reference_mode and
