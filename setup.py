f234de04a663a660185c784169bdefa5df7a96e1
Carl Thomé <carlthome@gmail.com>
2023-06-13 01:55:57 +0200

    Remove upper bound on matplotlib

--- setup.py
+++ setup.py
@@ -32,7 +32,7 @@ setup(
     extras_require={
         'display': ['matplotlib>=1.5.0',
                     'scipy>=1.0.0'],
-        'testing': ['matplotlib>=2.1.0,<3',
+        'testing': ['matplotlib>=2.1.0',
                     'decorator',
                     'pytest',
                     'pytest-cov',
