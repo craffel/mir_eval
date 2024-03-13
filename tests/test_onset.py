4d8518abd77c047fc71bbf914156c7a54abba4a7
Brian McFee <brian.mcfee@nyu.edu>
2024-03-13 11:27:42 -0400

    test_onset → pytest

--- tests/test_onset.py
+++ tests/test_onset.py
@@ -3,77 +3,77 @@ Unit tests for mir_eval.onset
 '''
 
 import numpy as np
+import pytest
 import json
 import mir_eval
 import glob
 import warnings
-import nose.tools
 
 A_TOL = 1e-12
 
 # Path to the fixture files
-REF_GLOB = 'data/onset/ref*.txt'
-EST_GLOB = 'data/onset/est*.txt'
-SCORES_GLOB = 'data/onset/output*.json'
+REF_GLOB = 'tests/data/onset/ref*.txt'
+EST_GLOB = 'tests/data/onset/est*.txt'
+SCORES_GLOB = 'tests/data/onset/output*.json'
+
+ref_files = sorted(glob.glob(REF_GLOB))
+est_files = sorted(glob.glob(EST_GLOB))
+sco_files = sorted(glob.glob(SCORES_GLOB))
+
+assert len(ref_files) == len(est_files) == len(sco_files) > 0
+
+file_sets = list(zip(ref_files, est_files, sco_files))
+
+@pytest.fixture
+def onset_data(request):
+    ref_f, est_f, sco_f = request.param
+    with open(sco_f, "r") as f:
+        expected_scores = json.load(f)
+    reference_onsets = mir_eval.io.load_events(ref_f)
+    estimated_onsets = mir_eval.io.load_events(est_f)
+
+    return reference_onsets, estimated_onsets, expected_scores
 
 
-def __unit_test_onset_function(metric):
-    with warnings.catch_warnings(record=True) as w:
-        warnings.simplefilter('always')
-        # First, test for a warning on empty onsets
-        metric(np.array([]), np.arange(10))
-        assert len(w) == 1
-        assert issubclass(w[-1].category, UserWarning)
-        assert str(w[-1].message) == "Reference onsets are empty."
-        metric(np.arange(10), np.array([]))
-        assert len(w) == 2
-        assert issubclass(w[-1].category, UserWarning)
-        assert str(w[-1].message) == "Estimated onsets are empty."
-        # And that the metric is 0
-        assert np.allclose(metric(np.array([]), np.array([])), 0)
+def test_onset_empty_warnings():
 
-    # Now test validation function - onsets must be 1d ndarray
-    onsets = np.array([[1., 2.]])
-    nose.tools.assert_raises(ValueError, metric, onsets, onsets)
-    # onsets must be in seconds (so not huge)
-    onsets = np.array([1e10, 1e11])
-    nose.tools.assert_raises(ValueError, metric, onsets, onsets)
-    # onsets must be sorted
-    onsets = np.array([2., 1.])
-    nose.tools.assert_raises(ValueError, metric, onsets, onsets)
+    with pytest.warns(UserWarning, match="Reference onsets are empty."):
+        mir_eval.onset.f_measure(np.array([]), np.arange(10))
 
+    with pytest.warns(UserWarning, match="Estimated onsets are empty."):
+        mir_eval.onset.f_measure(np.arange(10), np.array([]))
+    
+    with pytest.warns(UserWarning, match="onsets are empty"):
+        # Also verify that the score is 0
+        assert np.allclose(mir_eval.onset.f_measure(np.array([]), np.array([])), 0)
+
+
+@pytest.mark.xfail(raisses=ValueError)
+@pytest.mark.parametrize('onsets', [
+        np.array([[1., 2.]]),  # must be 1d ndarray
+        np.array([1e10, 1e11]),  # must not be huge
+        np.array([2., 1.]),  # must be sorted
+    ])
+def test_onset_fail(onsets):
+    mir_eval.onset.f_measure(onsets, onsets)
+
+def test_onset_match():
     # Valid onsets which are the same produce a score of 1 for all metrics
     onsets = np.arange(10, dtype=np.float64)
-    assert np.allclose(metric(onsets, onsets), 1)
+    assert np.allclose(mir_eval.onset.f_measure(onsets, onsets), 1.0)
 
 
-def __check_score(sco_f, metric, score, expected_score):
-    assert np.allclose(score, expected_score, atol=A_TOL)
-
-
-def test_onset_functions():
-    # Load in all files in the same order
-    ref_files = sorted(glob.glob(REF_GLOB))
-    est_files = sorted(glob.glob(EST_GLOB))
-    sco_files = sorted(glob.glob(SCORES_GLOB))
-
+@pytest.mark.parametrize("onset_data", file_sets, indirect=True)
+def test_onset_functions(onset_data):
+    reference_onsets, estimated_onsets, expected_scores = onset_data
     assert len(ref_files) == len(est_files) == len(sco_files) > 0
 
     # Unit tests
-    for metric in [mir_eval.onset.f_measure]:
-        yield (__unit_test_onset_function, metric)
-    # Regression tests
-    for ref_f, est_f, sco_f in zip(ref_files, est_files, sco_files):
-        with open(sco_f, 'r') as f:
-            expected_scores = json.load(f)
-        # Load in an example onset annotation
-        reference_onsets = mir_eval.io.load_events(ref_f)
-        # Load in an example onset tracker output
-        estimated_onsets = mir_eval.io.load_events(est_f)
-        # Compute scores
-        scores = mir_eval.onset.evaluate(reference_onsets, estimated_onsets)
-        # Compare them
-        for metric in scores:
-            # This is a simple hack to make nosetest's messages more useful
-            yield (__check_score, sco_f, metric, scores[metric],
-                   expected_scores[metric])
+    #for metric in [mir_eval.onset.f_measure]:
+    #    yield (__unit_test_onset_function, metric)
+    # Compute scores
+    scores = mir_eval.onset.evaluate(reference_onsets, estimated_onsets)
+    # Compare them
+    assert scores.keys() == expected_scores.keys()
+    for metric in scores:
+        assert np.allclose(scores[metric], expected_scores[metric], atol=A_TOL)
