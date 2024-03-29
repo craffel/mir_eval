b44cb8adc9917be053a11abfc201c658901b166d
Brian McFee <brian.mcfee@nyu.edu>
2024-03-29 15:04:50 -0400

    linting

--- mir_eval/chord.py
+++ mir_eval/chord.py
@@ -346,6 +346,8 @@ def reduce_extended_quality(quality):
 CHORD_RE = re.compile(
     r"""^((N|X)|(([A-G](b*|#*))((:(maj|min|dim|aug|1|5|sus2|sus4|maj6|min6|7|maj7|min7|dim7|hdim7|minmaj7|aug7|9|maj9|min9|11|maj11|min11|13|maj13|min13)(\((\*?((b*|#*)([1-9]|1[0-3]?))(,\*?((b*|#*)([1-9]|1[0-3]?)))*)\))?)|(:\((\*?((b*|#*)([1-9]|1[0-3]?))(,\*?((b*|#*)([1-9]|1[0-3]?)))*)\)))?((/((b*|#*)([1-9]|1[0-3]?)))?)?))$"""
 )  # nopep8
+
+
 def validate_chord_label(chord_label):
     """Test for well-formedness of a chord label.
 
